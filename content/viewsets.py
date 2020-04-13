8fd2c11ee01aa34adbb218a0cc331e1cb8a07050
marzieh <marziehsepehr@gmail.com>
2020-04-13 23:06:30 +0430

    remove unused stuff

--- content/viewsets.py
+++ content/viewsets.py
@@ -15,10 +15,4 @@ class ProductViewSet(viewsets.ModelViewSet):
         store = store_from_request(self.request)
         return super().get_queryset().filter(store=store)
 
-    @permission_classes([permissions.IsAdminUser])
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
-        return Response(serializer.data, status.HTTP_400_BAD_REQUEST)
+
