8f4f3b1bd1b8dc2f7a4f1acad4c68467bdcf12b6
marzieh <marziehsepehr@gmail.com>
2020-04-15 02:37:43 +0430

    add Owner to store

--- stores/models.py
+++ stores/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from users.models import StoreAdminUser
 
 class Store(models.Model):
     name = models.CharField(max_length=100)
     subdomain_prefix = models.CharField(max_length=100, unique=True)
+    owner = models.ForeignKey(StoreAdminUser,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
