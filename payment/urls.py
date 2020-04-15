7985140657c2bd80cacdd11bd17f8e9c988a63ad
marzieh <marziehsepehr@gmail.com>
2020-04-16 02:31:55 +0430

    add payment skelton for cutomer orders

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from payment.views import PaymentSubscribeView
+from payment.views import PaymentSubscribeView, PaymentView
 
 urlpatterns = [
     path('subscribe/<int:subscribe_id>/', PaymentSubscribeView.as_view(), name='subscribepayment'),
+    path('<int:orderid>', PaymentView.as_view(), name='payment'),
 
 ]
--- payment/views.py
+++ payment/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from membership.models import MemberShipOrder
+from order.models import Order
 
 
 class PaymentSubscribeView(APIView):
@@ -28,3 +29,24 @@ class PaymentSubscribeView(APIView):
             order.shipping_status = MemberShipOrder.UNSUCCESSFUL
             order.save(update_fields=['order_status'])
             return Response({'message': 'unsuccessful payment'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PaymentView(APIView):
+
+    def get(self, request, orderid):
+
+        """
+       Payment app skeleton
+       payment_result=0 ==> assume payment result is not ok
+       payment_result=1 ==>assume payment result is ok
+       """
+        payment_result = 1
+        order = Order.objects.get(id=orderid)
+        if payment_result:
+            order.order_status = Order.SUCCESSFUL
+            order.save(update_fields=['order_status'])
+            return Response({'status': 'successful payed'})
+        else:
+            order.order_status = Order.UNSUCCESSFUL
+            order.save(update_fields=['order_status'])
+            return Response({'status': 'successful payed'})
