cbde70a0904ed87c0170d30656f6f5df45407afa
marzieh <marziehsepehr@gmail.com>
2020-04-15 18:05:08 +0430

    modify backend to check request store just for admin page not regular user

--- users/backends.py
+++ users/backends.py
@@ -10,18 +10,20 @@ from stores.models import Store
 class StoreBackend(ModelBackend):
 
     def authenticate(self, request, username, password):
-        store_name = store_from_request(request)
 
         try:
             user = StoreUser.objects.get(email=username)
             if user.check_password(password) is True:
                 # check authenticated user is owner of store or not
-                try:
-                    is_owner = Store.objects.get(owner__user=user, name=store_name)
+                if user.is_staff:
+                    store_name = store_from_request(request)
+                    try:
+                        is_owner = Store.objects.get(owner__user=user, name=store_name)
+                        return user
+                    except Store.DoesNotExist:
+                        raise forms.ValidationError(
+                            _("You are not the owner of {} store. You don't have access to this page").format(store_name))
+                else:
                     return user
-                except Store.DoesNotExist:
-                    raise forms.ValidationError(
-                        _("You are not the owner of {} store. You don't have access to this page").format(store_name))
-
         except StoreUser.DoesNotExist:
             pass
